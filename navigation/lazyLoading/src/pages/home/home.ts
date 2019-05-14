016894516313f8e85054db1f39d583a90b320e4c
Maria-Alexandra Irimescu <irimescu.mariaalexandra@gmail.com>
2019-05-14 15:14:34 +0300

    Created LazyLoading project

--- navigation/lazyLoading/src/pages/home/home.ts
+++ navigation/lazyLoading/src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SecondPage } from '../second/second';
 
 @Component({
   selector: 'page-home',
@@ -12,11 +11,4 @@ export class HomePage {
 
   }
 
-  navigateToSecondPage(): void {
-    //Without using Lazy Loading
-    // this.navCtrl.push(SecondPage);
-
-    //Using Lazy Loading
-    this.navCtrl.push('secondPage');
-  }
 }
