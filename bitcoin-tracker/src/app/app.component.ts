6eafdb50c63a7fc3f65ec3499b33730e52ca8706
Maria-Alexandra Irimescu <irimescu.mariaalexandra@gmail.com>
2019-05-15 14:19:07 +0300

    Implemented the bitcoin tracker

--- bitcoin-tracker/src/app/app.component.ts
+++ bitcoin-tracker/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  //Lazy loaded the Bitcoin
+  rootPage: string = 'BitcoinPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
