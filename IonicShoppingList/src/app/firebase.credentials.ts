a9d08575117b9c7ab833ec3667d7055262b2ce3a
Maria-Alexandra Irimescu <irimescu.mariaalexandra@gmail.com>
2019-05-16 16:19:15 +0300

    Installed package for Firebase

---
+++ IonicShoppingList/src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCqtY40ja2tbjJlhF_1Z3WfZIekjCuj0cY",
+  authDomain: "shoppinglist-c0238.firebaseapp.com",
+  databaseURL: "https://shoppinglist-c0238.firebaseio.com",
+  projectId: "shoppinglist-c0238",
+  storageBucket: "shoppinglist-c0238.appspot.com",
+  messagingSenderId: "928074240144"
+}
--- IonicShoppingList/src/pages/home/home.ts
+++ IonicShoppingList/src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
+//By adding '@IonicPage()' we can lazy loading the HomePage
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
