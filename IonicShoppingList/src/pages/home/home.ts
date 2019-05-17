1ab2e1e2425ecca8aa4a0d0af8ce21861d9329d9
Maria-Alexandra Irimescu <irimescu.mariaalexandra@gmail.com>
2019-05-17 13:21:38 +0300

    Initial create beep application

--- IonicShoppingList/src/pages/home/home.ts
+++ IonicShoppingList/src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
 
+import { Item } from '../../models/item/item.model';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
 //By adding '@IonicPage()' we can lazy loading the HomePage
 @IonicPage()
 @Component({
@@ -9,8 +13,25 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable<Item[]>;
+  shopping: Item[];
 
+  constructor(public navCtrl: NavController,
+              private shoppingListService: ShoppingListService) {
+
+    // this.shoppingList$ = this.shoppingListService
+    //   .getShoppingList() // get db list
+    //   .snapshotChanges() // get both key and value
+    //     .map(changes => {
+    //       return changes.map(c => ({
+    //         key: c.payload.key, ...c.payload.val()
+    //       }));
+    //     })
+
+    //     console.log(this.shoppingList$);
+              this.shoppingListService.getShoppingList().snapshotChanges();
   }
+
+
 
 }
--- beep/src/app/app.module.ts
+++ beep/src/app/app.module.ts
@@ -3,31 +3,27 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
-import { FIREBASE_CONFIG } from './firebase.credentials';
-import { ShoppingListService } from '../services/shopping-list/shopping-list.service';
+import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp
+    MyApp,
+    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(FIREBASE_CONFIG),
-    AngularFireDatabaseModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    ShoppingListService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
